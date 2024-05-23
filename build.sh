b87f2a7677f2ec34557872772adbf509c107b350
Dustin Scarberry <bitsnbytes1001@gmail.com>
2024-05-23 00:19:24 -0400

    Node 22 and version updates

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-node18Version=node:18.18.0-alpine
-node20Version=node:20.8.0-alpine
+node18Version=node:18.20.3-alpine
+node20Version=node:20.13.1-alpine
+node22Version=node:22.2.0-alpine
 
 docker login
 
 # build and push base docker images
 
-docker build -t dustinscarberry/express:node18 --build-arg NODE_VERSION=$node18Version -f images/Dockerfile .
+docker buildx build --platform linux/amd64 -t dustinscarberry/express:node18 --build-arg NODE_VERSION=$node18Version -f images/Dockerfile .
 docker push dustinscarberry/express:node18
 
-docker build -t dustinscarberry/express:node20 --build-arg NODE_VERSION=$node20Version -f images/Dockerfile .
+docker buildx build --platform linux/amd64 -t dustinscarberry/express:node20 --build-arg NODE_VERSION=$node20Version -f images/Dockerfile .
 docker push dustinscarberry/express:node20
+
+docker buildx build --platform linux/amd64 -t dustinscarberry/express:node22 --build-arg NODE_VERSION=$node22Version -f images/Dockerfile .
+docker push dustinscarberry/express:node22
